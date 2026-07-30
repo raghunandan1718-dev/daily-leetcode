7f5bf83a4eed0859180f245a65d832708649e69d
raghunandan1718-dev <raghunandan.1718@gmail.com>
2026-07-30 22:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (66.51%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumPushes(String word) {
+
+        int push =0;
+        int n = word.length();
+
+        for(int i=0; i<word.length(); i++){
+            push += (i/8)+1;
+        }        
+
+        return push;
+    }
+}
